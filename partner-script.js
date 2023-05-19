7f0a5e711543c3a23fa881f9570fe2645e127d80
richNovoa <56084025+richardNovoa@users.noreply.github.com>
2023-05-18 22:44:30 -0400

    Update partner-script.js

--- partner-script.js
+++ partner-script.js
@@ -1,31 +1,28 @@
-
-window.addEventListener('message', function(event) {
-  if (event.origin === 'http://localhost:3000/') {
-    var eventData = event.data; 
+window.addEventListener("message", function (event) {
+  if (event.origin === "http://localhost:3000/") {
+    var eventData = event.data;
     var iframeURL = eventData.iframeURL;
-//Start
-mixpanel.time_event("Time on Page");
-//End
-document.addEventListener(`visibilitychange`, function () {
-  if (document.visibilityState === "hidden") {
-    mixpanel.set_config({ api_transport: "sendBeacon" });
-    mixpanel.track("Time on Page", {
-      source: iframeURL
-    });
-    mixpanel.people.set({ foo: "bar" });
-    x++;
-  } else if (document.visibilityState === "pagehide") {
-    mixpanel.set_config({ api_transport: "sendBeacon" });
-    mixpanel.track("Time on Page");
-    mixpanel.people.set({ foo: "bar" });
-  } else if (document.visibilityState === "visible") {
+
+    //EVENTS
+
+    //Start
     mixpanel.time_event("Time on Page");
+    //End
+    document.addEventListener(`visibilitychange`, function () {
+      if (document.visibilityState === "hidden") {
+        mixpanel.set_config({ api_transport: "sendBeacon" });
+        mixpanel.track("Time on Page", {
+          source: iframeURL,
+        });
+        mixpanel.people.set({ foo: "bar" });
+        x++;
+      } else if (document.visibilityState === "pagehide") {
+        mixpanel.set_config({ api_transport: "sendBeacon" });
+        mixpanel.track("Time on Page");
+        mixpanel.people.set({ foo: "bar" });
+      } else if (document.visibilityState === "visible") {
+        mixpanel.time_event("Time on Page");
+      }
+    });
   }
 });
-  }}
-
-
-
-
-
-
