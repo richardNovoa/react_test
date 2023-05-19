cd134d082826d666d1098e9f09257b35a05758f4
richNovoa <56084025+richardNovoa@users.noreply.github.com>
2023-05-19 12:37:29 -0400

    Update partner-script.js

--- partner-script.js
+++ partner-script.js
@@ -1,3 +1,5 @@
+// Methods
+
 function triggerEvent1() {
   var eventData = {
     eventName: "Click",
@@ -15,3 +17,16 @@ function triggerEvent2() {
   window.parent.postMessage(eventData, "*");
   console.log("Event 2");
 }
+
+// Tagging
+var ClickEvent1 = document.getElementById("event1");
+var ClickEvent2 = document.getElementById("event2");
+
+//Listening
+ClickEvent1.addEventListener("click", function () {
+  triggerEvent1;
+});
+
+ClickEvent2.addEventListener("click", function () {
+  triggerEvent2;
+});
